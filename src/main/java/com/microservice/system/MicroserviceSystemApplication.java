80d568505ddf938241b8cfc9cf60d7e4fae23b68
Tzuy <tranlythanhduy284@gmail.com>
2024-11-11 15:13:58 +0700

    Initial commit

---
+++ src/main/java/com/microservice/system/MicroserviceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservice/system/MicroserviceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
